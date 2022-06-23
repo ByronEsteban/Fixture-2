8324b45e5a1b80b1300dbd99df08e7760c866471
ByronEsteban <byronestebanum@gmail.com>
2022-06-23 12:38:27 -0300

    Fixture

--- script.js
+++ script.js
@@ -42,3 +42,75 @@ for (let g of groups) {
   div.append(table);
   document.querySelector('.tables').append(div);
 }
+// let matches = document.getElementById('matches')
+// for (let g of groups) {
+//   let i = 0;
+//   for (let team of g[1]) {
+//     let a = document.createElement('div');
+//     a.id = 'a';
+//     a.textContent = team;
+//     if (i % 2 != 0) {
+//       let res = document.createElement('div');
+//       res.id = 'res';
+//       let b = ' vs';
+//       matches.lastElementChild.append(b)
+//       matches.lastElementChild.append(a)
+//     }
+//
+//     matches.append(a);
+//     i++;
+//   }
+// }
+//
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let local = event.target.children[0].textContent;
+  let visitante = event.target.children[3].textContent;
+  let golesLocal = event.target.children[1].value;
+  let golesVisitante = event.target.children[2].value;
+
+  let ganoLocal = false;
+  let ganoVisitante = false;
+  let empate = false;
+  if (golesLocal > golesVisitante) ganoLocal = true;
+  else if (golesLocal < golesVisitante) ganoVisitante = true
+  else if (golesLocal == golesVisitante) empate = true;
+
+
+  // console.log(`${local} ${golesLocal} | ${golesVisitante} ${visitante}`);
+  let tds = document.querySelectorAll('td');
+  for (let td of tds) {
+    if (td.textContent == local || td.textContent == visitante) {
+      // si empatan PE + 1
+      if (empate) {
+        td.nextElementSibling.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.textContent) + 1;
+      }
+      // PJ + 1
+      td.nextElementSibling.textContent = parseInt(td.nextElementSibling.textContent) + 1;
+    }
+    if (td.textContent == local) {
+      if (ganoLocal) {
+        td.parentElement.children[2].textContent = parseInt(td.parentElement.children[2].textContent) + 1;
+      } else if (ganoVisitante) {
+        td.parentElement.children[4].textContent = parseInt(td.parentElement.children[4].textContent) + 1;
+      }
+      // GF del local
+      td.parentElement.children[5].textContent = parseInt(td.parentElement.children[5].textContent) + parseInt(golesLocal);
+      // GC del local
+      td.parentElement.children[6].textContent = parseInt(td.parentElement.children[6].textContent) + parseInt(golesVisitante);
+    }
+    if (td.textContent == visitante) {
+      if (ganoLocal) {
+        td.parentElement.children[4].textContent = parseInt(td.parentElement.children[4].textContent) + 1;
+      } else if (ganoVisitante) {
+        td.parentElement.children[2].textContent = parseInt(td.parentElement.children[2].textContent) + 1;
+      }
+      // GF del visitante
+      td.parentElement.children[5].textContent = parseInt(td.parentElement.children[5].textContent) + parseInt(golesVisitante);
+      // GC del visitante
+      td.parentElement.children[6].textContent = parseInt(td.parentElement.children[6].textContent) + parseInt(golesLocal);
+    }
+
+  }
+}
